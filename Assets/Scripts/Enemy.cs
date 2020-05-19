d3baf4a109bccac82140fc9c9adaf75e99292315
Sami Sipari <sami.sipari@tuni.fi>
2020-05-19 16:45:10 +0300

    Setup code for making the prefabs
    The code is commented and you can check them out in the scripts folder under the Assets folder

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // The maximum X-axis value of the enemy's position
+    public float _maxX;
+    // The minimum X-axis value of the enemy's position
+    public float _minX;
+    // The enemy's speed
+    public float _speed;
+    // Is the enemy moving left? (if false it is moving right)
+    public bool _isMovingLeft;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if the enemy is moving left
+        if (_isMovingLeft)
+        {
+            // Move the enemy to the left according to his speed. Remember to always use Time.DeltaTime!
+            transform.position -= new Vector3(_speed, 0) * Time.deltaTime;
+
+            // Check if the enemy is beyond its minimum X-axis value.
+            if (_minX > transform.position.x)
+            {
+                // If it is, make the moving left boolean false and move the player to its minimum X-axis value.
+                _isMovingLeft = false;
+                transform.position = new Vector3(_minX, transform.position.y);
+            }
+        }
+        else
+        {
+            // Move the enemy to the right according to his speed. Remember to always use Time.DeltaTime!
+            transform.position += new Vector3(_speed, 0) * Time.deltaTime; 
+            
+            if (_maxX < transform.position.x)
+            {
+                // If it is, make the moving left boolean true and move the player to its maximum X-axis value.
+                _isMovingLeft = true;
+                transform.position = new Vector3(_maxX, transform.position.y);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float _speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Take in the users input.
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Combine them into a Vector3
+        // Vector3 can be initialised with only the x and y -axis, 
+        // which we will do since we have no need for the z-axis
+        Vector3 input = new Vector3(horizontalInput, verticalInput);
+
+        // Multiply the current input values by the speed parameter
+        Vector3 movement = input * _speed;
+
+        // Multiply the movement vector by the speed parameter and then Time.Deltatime.
+        // Time.DeltaTime gives you the time between the current and previous frame.
+        // Using it makes your movement not be affected by the current frames per second.
+        // You can test how it affects the movement by removing it from the code.
+        //             Only this bit -> |        ↓       |
+        Vector3 deltaMovement = movement * Time.deltaTime ;
+
+        // Add the deltaMovement to the current position
+        Vector3 newPosition = transform.position + deltaMovement;
+
+        // Change the current position to the new position
+        transform.position = newPosition;
+    }
+}
